9a7df9ed84af5f8f140276d3303fba622ea3f5c6
Eduardo Vieira Oliveira <eduardovieiraoliveiraa@gmail.com>
2021-04-06 20:03:58 -0300

    Criação classe OrderItem

--- src/main/java/com/eduardo/Webservices/config/TestConfig.java
+++ src/main/java/com/eduardo/Webservices/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.eduardo.Webservices.entities.Category;
 import com.eduardo.Webservices.entities.Order;
+import com.eduardo.Webservices.entities.OrderItem;
 import com.eduardo.Webservices.entities.Product;
 import com.eduardo.Webservices.entities.User;
 import com.eduardo.Webservices.entities.enums.OrderStatus;
 import com.eduardo.Webservices.repositories.CategoryRepository;
+import com.eduardo.Webservices.repositories.OrderItemRepository;
 import com.eduardo.Webservices.repositories.OrderRepository;
 import com.eduardo.Webservices.repositories.ProductRepository;
 import com.eduardo.Webservices.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -67,6 +72,14 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
 		
 	}
 
